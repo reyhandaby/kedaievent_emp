95415a5256adf5e55b92d9b589c783e7b5402a81
Reyhandra Abyonas <reyhandabyonas@gmail.com>
2025-10-26 10:36:07 +0700

    Update API_BASE URL to production server

--- client/src/app/lib/api.ts
+++ client/src/app/lib/api.ts
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
-export const API_BASE = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
+export const API_BASE = process.env.NEXT_PUBLIC_API_URL || 'https://kedaievent-emp-server.vercel.app/';
 export const api = axios.create({ baseURL: `${API_BASE}/api` });
